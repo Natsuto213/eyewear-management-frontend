e24964c4356ea2079ebf8daa9c550c332dc725e2
Kien <phamkien942402@gmail.com>
2026-02-26 19:12:06 +0700

    fix confirmpage

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api-eyewear.purintech.id.vn/",
+  baseURL: "https://api-eyewear.purintech.id.vn",
   headers: { "Content-Type": "application/json" },
 });
 
--- src/views/AccountPage.tsx
+++ src/views/AccountPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { apiGetMyInfo, apiUpdateMyInfo } from "../lib/userApi";
+import { apiGetMyInfo, apiUpdateMyInfo } from "../app/userApi";
 
 
 type FormState = {
