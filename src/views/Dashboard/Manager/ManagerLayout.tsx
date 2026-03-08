a571c8ad17b48571f40d5d53040568702588b934
Natsuto213 <Natsuto213@gmail.com>
2026-03-08 16:42:24 +0700

    small adjust

--- src/views/Dashboard/Manager/ManagerLayout.tsx
+++ src/views/Dashboard/Manager/ManagerLayout.tsx
@@ -2,15 +2,18 @@ import { Outlet } from 'react-router';
 import { ProtectedLayout } from '@/components/dashboard/ProtectedLayout';
 import { managerTabs } from '@/components/dashboard/navigation';
 import { Navigate } from 'react-router-dom';
+
 export const ManagerLayout = () => {
     const userSaved = localStorage.getItem("user");
     if (!(userSaved)) {
         return <Navigate to="/login" />
     }
+    
     const userData = JSON.parse(userSaved)
     if (userData.role !== "MANAGER") {
         return <Navigate to="/login" />
     }
+
     return (
         <>
             <ProtectedLayout tabs={managerTabs} role="manager" defaultTab="dashboard" userName={userData.name}>
