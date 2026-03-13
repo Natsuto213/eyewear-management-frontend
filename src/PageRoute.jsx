7d5702204cbaf19d037e893b271977ecc7541907
Kien <phamkien942402@gmail.com>
2026-03-13 23:35:12 +0700

    Customer history

--- src/PageRoute.jsx
+++ src/PageRoute.jsx
@@ -64,8 +64,9 @@ export default function PageRoute() {
 
         <Route path="/profile" element={<ProfilePage />}>
           <Route path="account" element={<Account />} />
-          <Route path="orders/:orderId" element={<OrderDetailCustomer />} />
+          
         </Route>
+        <Route path="/profile/orders/:orderId" element={<OrderDetailCustomer />} />
 
        
         <Route path="/sales" element={<SalesStaffLayout />}>
